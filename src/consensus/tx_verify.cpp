e8b83dc1063d68bfab47697ce42aec93a7f2dc03
deomaius <samuel.jj.gosling@gmail.com>
2024-04-01 07:41:43 +0100

    bump cb size limit

--- src/consensus/tx_verify.cpp
+++ src/consensus/tx_verify.cpp
@@ -376,7 +376,7 @@ bool CheckTransaction(const CTransaction& tx, CValidationState &state, bool fChe
 
     if (tx.IsCoinBase())
     {
-        if (tx.vin[0].scriptSig.size() < 2 || tx.vin[0].scriptSig.size() > 100)
+        if (tx.vin[0].scriptSig.size() < 2 || tx.vin[0].scriptSig.size() > 175)
             return state.DoS(100, false, REJECT_INVALID, "bad-cb-length");
 
         if (AreCoinbaseCheckAssetsDeployed()) {
